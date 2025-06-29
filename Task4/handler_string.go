356b1b2717365751adbb5fe07f9b8f0e7b54e4ff
Fanaenic <korapetr@gmail.com>
2025-06-29 20:55:56 +0300

    summer_practise

--- Task4/handler_string.go
+++ Task4/handler_string.go
@@ -9,6 +9,6 @@ func main() {
 	var text string = "Olofmeister, I love You"
 
 	fmt.Println("Количество  символов:", len(text), "символов")
-	fmt.Println("Есть ли 'I'?", strings.Contains(text, "I"))
+	fmt.Println("Есть 'I'?", strings.Contains(text, "I"))
 	fmt.Println("Верхний регистр:", strings.ToUpper(text))
 }
